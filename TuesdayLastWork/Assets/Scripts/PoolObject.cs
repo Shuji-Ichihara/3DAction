2b46b1333e2d7142dcf9358828a9d5fafd225000
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-02-06 12:29:39 +0900

    Create PoolObject
    オブジェクトプールに必要な機能を追加した。

---
+++ TuesdayLastWork/Assets/Scripts/PoolObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// オブジェクトプールに必要な機能
+/// </summary>
+
+public class PoolObject : MonoBehaviour
+{
+    Transform _transform;
+
+    public void InitItem(Vector3 spwanPos)
+    {
+        _transform = GetComponent<Transform>();
+        //アイテムの生成位置を指定
+        _transform.position = spwanPos;
+        //アイテムをアクティブにする
+        this.gameObject.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "GameOver")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
