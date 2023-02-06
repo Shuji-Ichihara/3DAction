d5d2096c674858b1d06be79b3e4f3e592c6e99e1
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-02-06 12:30:15 +0900

    Create Generate
    オブジェクトを生成するスクリプトを作成した。

---
+++ TuesdayLastWork/Assets/Scripts/Generate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オブジェクトプール作成
+/// </summary>
+public class Generate : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _obstacle = null;
+
+    private List<PoolObject> _list_poolObject = new List<PoolObject>();
+
+    [SerializeField]
+    private int _max = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _max; i++)
+        {
+            PoolObject _poolObject;
+
+            _poolObject = Instantiate(_obstacle).GetComponent<PoolObject>();
+            _poolObject.transform.parent = transform;
+            _poolObject.gameObject.SetActive(false);
+            _list_poolObject.Add(_poolObject);
+        }
+    }
+
+    /// <summary>
+    /// _list_poolObject 内の非アクティブのオブジェクトを探す
+    /// </summary>
+    /// <param name="spawnPos"></param>
+    public void GeneratePoolObject(Vector3 spawnPos)
+    {
+        for (int i = 0; i < _list_poolObject.Count; i++)
+        {
+            if (false == _list_poolObject[i].gameObject.activeSelf)
+            {
+                // 非アクティブのオブジェクトを生成
+                _list_poolObject[i].InitItem(spawnPos);
+                break;
+            }
+        }
+    }
+}
